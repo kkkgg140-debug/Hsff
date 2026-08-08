5db50f3086a368a6c8f1fdecce5aad4e4aab3bad
kkkgg140-debug <kkkgg140@gmail.com>
2026-08-07 22:49:34 -0300

    main.py

--- Main.py
+++ Main.py
@@ -24,5 +24,7 @@ def home():
 def config():
     return jsonify(CONFIG_HS)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+import os
+   if __name__ == '__main__':
+       port = int(os.environ.get("PORT", 8080))
+       app.run(host='0.0.0.0', port=port)
